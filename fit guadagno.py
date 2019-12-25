021ee5287d8fd959cfd5ee021f8e149d038e0e02
giulio99 <56739190+giulio99@users.noreply.github.com>
2019-12-25 02:05:30 +0100

    Update fit guadagno.py

--- fit guadagno.py
+++ fit guadagno.py
@@ -27,4 +27,26 @@ Af=1/np.sqrt(1+(ff/ft)**2)
 
 plt.plot(ff,Af, color='blue')
 
+#simulazione per varie frequenze 
+#ATTENZIONE, fa un sacco di operazioni, non aumentare il numero di dispari, ff , y e x altrimenti ti si pianta il computer
+dispari=np.arange(1, 5000, 2)
+
+ck=2/(dispari*np.pi)
+y=np.empty((len(ff), 100))
+
+for j in range(len(ff)):
+    x=np.linspace(-0.5/ff[j], 1.2/ff[j],100)
+    for i in range(len(x)):
+        Ak=1/np.sqrt(1+((ww[j]*dispari)/wt)**2)
+        fi=np.arctan(-(ww[j]*dispari)/wt)
+        a=(ck*Ak*np.sin(ww[j]*dispari*x[i] + fi)).sum()
+        y[j][i]=a
+
+Ampl=np.empty(len(ff))
+
+for j in range(len(ff)):
+    Ampl[j]=np.abs(max(y[j])-min(y[j]))
+
+plt.plot(ff, Ampl, color='green')
+
 plt.show()
