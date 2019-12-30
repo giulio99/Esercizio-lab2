adea5f3ed7cee49948fcf8db4f158c21047fb9de
blancabru <58997940+blancabru@users.noreply.github.com>
2019-12-30 17:00:14 +0100

    Update integratore trig.py

--- integratore trig.py
+++ integratore trig.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib 
 import matplotlib.pyplot as plt
-import plotly.offline as ply
-
+import pylab
+#import plotly.offline as ply
+#integra la triangolare
 
 dispari=np.arange(1, 50, 2)
 f=np.array([ 5, 20, 50, 100, 300, 500,1000, 3000,5000, 10000])
@@ -31,13 +32,22 @@ fig.subplots_adjust(hspace=0)
 for i in range(5):
     for j in range(2):
         plt.sca(axs[i, j])
-        plt.plot((np.linspace(0, 4/f[2*i+j],1000)), y[2*i+j])
+        plt.plot((np.linspace(0, 4/f[2*i+j],1000)), y[2*i+j], label='f=%d Hz'%(f[2*i+j]))
         plt.xticks(np.arange(0,5/f[2*i+j], 1/f[2*i+j]), [0,1,2,3,4])
         plt.xlim(-0.1/f[2*i+j], 4/f[2*i+j]+0.1/f[2*i+j])
+        pylab.xlabel('Time [T]')
+        plt.legend(loc = 'upper right')
         
 for i in range(5):
     for j in range(2):
         if(i!=4):
             plt.setp(axs[i][j].get_xticklabels(), visible=False)
+            
+# add a big axis, hide frame
+fig.add_subplot(111, frameon=False)
+# hide tick and tick label of the big axis
+plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
+#plt.xlabel("common X")
+plt.ylabel("Simulated Signal [arb.un.]")
 
 plt.show()
